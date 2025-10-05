156d490b42ec370158f26bc184005f1346013dbd
奥田 雅基 <72392151+MASAKIOKUDA-eng@users.noreply.github.com>
2025-10-05 22:03:48 +0900

    エントリ設定を整理し、srcプロパティに統合しました。

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -8,32 +8,22 @@ module.exports = {
     '@mitsuharu/vivliostyle-theme-noto-sans-jp@0.1.4',
     'theme/theme-custom',
   ],
-  entry: [
-    // 目次
-    'index.md',
-    // はじめに
-    'preface.md',
-    // 各章の原稿
-    'docs/section1.md', //設計時のチームスキルに応じてサービスを選ぶ
-    'docs/section2.md', //陳腐化しない構成図を書いてみる
-    'docs/section3.md', //VPCで利用するアドレス範囲は慎重に
-    'docs/section4.md', //リソース名は誰もが分かる名称を付ける
-    'docs/section5.md', //セキュリティグループの設定は柔軟性を持たせる
-    // おわりに
-    'epilogue.md',
-    // 付録
-    'appendix.md',
-    // 著者紹介
-    'authors.md',
-    // 奥付
-    'colophon.md',
-  ],
+    src: [
+      { front: 'cover/front.png' },
+      'index.md',
+      'preface.md',
+      'docs/section1.md',
+      'docs/section2.md',
+      'docs/section3.md',
+      'docs/section4.md',
+      'docs/section5.md',
+      'authors.md',
+      'colophon.md',
+      { back: 'cover/back.png' }
+    ],
   entryContext: './manuscripts',
   output: ['output/ebook.pdf'],
   workspaceDir: '.vivliostyle',
   toc: false,
-  cover: {
-    front: 'cover/front.png',
-    back: 'cover/back.png'
-  },
+  }
 }
